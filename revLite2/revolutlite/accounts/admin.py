be2f1cd20566f4ec3dd4dea12b5832de5c3027ef
DimitarAtanasov92 <141042344+DimitarAtanasov92@users.noreply.github.com>
2025-04-10 04:23:47 -0700

    Delete revLite2/revolutlite directory

--- revLite2/revolutlite/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'is_verified', 'needs_manual_review', 'confidence_score')
-    list_filter = ('is_verified', 'needs_manual_review')
